0e628eafec50c01f765a537820d5ae21ee541189
Fabricio2407 <fabriciocduarte2413@gmail.com>
2026-06-12 20:58:42 -0300

    feat: cria contexto global de alunos

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AlunosProvider } from './context/AlunosContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AlunosProvider>
+      <App />
+    </AlunosProvider>
   </StrictMode>,
 )
